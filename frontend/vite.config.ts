0c59015769abc5fe75e02253bbf6ce4f51029822
Dardaul Hoque <rudro1152@gmail.com>
2023-08-12 19:10:21 +0600

    add babel plugin for solidjs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
+import { undestructurePlugin } from "babel-plugin-solid-undestructure"
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
+    ...undestructurePlugin(),
     /* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
@@ -15,6 +17,6 @@ export default defineConfig({
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
-});
+})
