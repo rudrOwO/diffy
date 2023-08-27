db78b09ee856f8f8f54a00de9ddf69ad72d7f610
Dardaul Hoque <rudro1152@gmail.com>
2023-08-27 18:52:38 +0600

    navigation bugfix

--- frontend/src/components/diff-renderer.tsx
+++ frontend/src/components/diff-renderer.tsx
@@ -3,10 +3,13 @@ import * as Diff2Html from "diff2html"
 import { Component, createSignal } from "solid-js"
 import Button from "./ui/button"
 import { IoArrowBackCircleSharp } from "solid-icons/io"
+import { useNavigate } from "@solidjs/router"
 
 const [diffString, setDiffString] = createSignal("")
 
 const DiffRenderer: Component<{}> = () => {
+  const navigate = useNavigate()
+
   const addDiffHtml = (element: HTMLDivElement) => {
     var diffHtml = Diff2Html.html(diffString(), {
       drawFileList: true,
@@ -21,9 +24,9 @@ const DiffRenderer: Component<{}> = () => {
       <div class="flex flex-col justify-evenly relative m-3">
         <div class="sticky left-2 top-2 my-2">
           <Button
-            title="Go Back"
-            onClick={() => window.history.back()}
-            icon={<IoArrowBackCircleSharp size="1rem" />}
+            title="Menu"
+            onClick={() => navigate("/")}
+            icon={<IoArrowBackCircleSharp size="1.25rem" />}
           />
         </div>
         <div ref={addDiffHtml} />
